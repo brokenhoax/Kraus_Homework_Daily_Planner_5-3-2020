b4df5673e5435d5056f099e4fa621a8fc3467cc0
brokenhoax <brokenhoax@outlook.com>
2020-05-04 15:16:29 -0500

    Added Table and Buttons

--- app.js
+++ app.js
@@ -2,13 +2,31 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const dateTime = document.getElementById('date-time');
+const saveButton = document.querySelector('.save-button');
 // const deleteButton = document.querySelector('.todo-trash')
 
 //Event Listeners
 todoButton.addEventListener('click', addToDo);
 todoList.addEventListener('click', deleteCheck);
+saveButton.addEventListener('click', scheduleToDo);
+
+//Current Date & Time
+console.log(moment());
+
+onload=function getDateAndTime() {
+    var NowMoment = moment().format('dddd MMMM Do YYYY');
+    var eDisplayMoment = document.getElementById('displayMoment');
+    eDisplayMoment.innerHTML = NowMoment;
+}
+
 
 //Functions
+
+// function getDateTime () {
+//     moment().format('MMMM Do YYYY, h:mm:ss a');
+// }
+
 
 function addToDo (event) {
     // Prevent form from submitting
@@ -39,6 +57,12 @@ function addToDo (event) {
     todoInput.value = "";
 }
 
+function scheduleToDo (event) {
+        // Prevent form from submitting
+        event.preventDefault();
+        console.log('goodbye')
+}
+
 function deleteCheck(e){
     const item = e.target;
     console.log(e.target)
@@ -47,7 +71,7 @@ function deleteCheck(e){
         const todoToRemove = item.parentElement;
         todoToRemove.remove();
     }
-    //Check Mark
+    //Check Mark Complete
     if(item.classList [0] === 'complete-btn') {
         const completedTodo = item.parentElement;
         completedTodo.classList.toggle('completed');
